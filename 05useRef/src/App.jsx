7bb470ba1ff73e5a1f6c997cf474d2bb33bc54eb
Tahirul Islam <5566tahirul@gmail.com>
2025-08-26 11:45:17 +0530

    useRef hook

--- 05useRef/src/App.jsx
+++ 05useRef/src/App.jsx
@@ -1,36 +1,34 @@
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 
 function App() {
-  // const [name, setName] = useState("");
-  // const prevName = useRef("");
+  // const username = document.getElementById('username');
+  // const password = document.getElementById('password');
 
-  // useEffect(() => {
-  //   prevName.current = name; // store previous value
-  // }, [name]);
+  const username = useRef(null);
+  const password = useRef(null);
 
-  // return (
-  //   <div>
-  //     <input value={name} onChange={(e) => setName(e.target.value)} />
-  //     <p>Current Name: {name}</p>
-  //     <p>Previous Name: {prevName.current}</p>
-  //   </div>
-  // );
-
-  
-
-
-//   const inputRef = useRef(null);
-
-// <input ref={inputRef} type="text" />;
-
-// // Access common DOM functions:
-// inputRef.current.focus();       // Focus input
-// inputRef.current.blur();        // Remove focus
-// inputRef.current.click();       // Programmatically click
-// inputRef.current.value = "Hi";  // Set value
-// inputRef.current.style.color = "red";  // Change style
-
-
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(username.current.value , password.current.value);
+    
+    
+  }
+  return(
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <form onSubmit={handleFormSubmit} className="flex flex-col gap-5 p-5 rounded-md shadow bg-gray-50">
+        <h1 className="text-center text-3xl font-bold">Login</h1>
+        <input type="text" ref={username}
+        className="outline-none border-3 rounded-md px-4 py-1.5 font-semibold border-blue-500" 
+        />
+        <input type="password"  ref={password}
+        className="outline-none border-3 rounded-md px-4 py-1.5 font-semibold border-blue-500" 
+        />
+        <button
+        className="px-4 py-1.5 rounded-md bg-green-400 font-bold cursor-pointer"
+        >Submit</button>
+      </form>
+    </div>
+  )
 
 }
 
